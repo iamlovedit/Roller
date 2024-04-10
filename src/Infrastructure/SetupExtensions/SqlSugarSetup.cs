6134c05951c9d66237c9c389514ae6b2df9ef61d
Young <iamlovedit@163.com>
2024-04-10 14:26:22 +0800

    update codes

---
+++ src/Infrastructure/SetupExtensions/SqlSugarSetup.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Infrastructure.SetupExtensions;
+
+public static class SqlSugarSetup
+{
+    public static void AddSqlSugarSetup(this IServiceCollection services, IConfiguration configuration,
+        IHostEnvironment hostEnvironment)
+    {
+        ArgumentNullException.ThrowIfNull(hostEnvironment);
+        ArgumentNullException.ThrowIfNull(configuration);
+        ArgumentNullException.ThrowIfNull(services);
+    }
+}
