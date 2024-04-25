fe77f36d95df9513ff002eb1c3c5979bc0539eeb
Young <iamlovedit@163.com>
2024-04-25 21:39:33 +0800

    updated test controller

--- src/Roller.Tutorial/Controller/V1/TutorialController.cs
+++ src/Roller.Tutorial/Controller/V1/TutorialController.cs
@@ -1,7 +1,17 @@
-﻿using Roller.Infrastructure;
+﻿using Asp.Versioning;
+using Microsoft.AspNetCore.Mvc;
+using Roller.Infrastructure;
 
 namespace Roller.Tutorial.Controller.V1;
 
-public class TutorialController : RollerControllerBase
+[ApiVersion("1.0")]
+[Route("tutorial/{version:apiVersion}")]
+public class TutorialController(ILogger<TutorialController> logger) : RollerControllerBase
 {
+    [HttpGet("hello")]
+    public async Task<MessageData<string>> HelloAsync()
+    {
+        logger.LogInformation("hello world");
+        return await Task.Run(() => Success("hello world"));
+    }
 }
