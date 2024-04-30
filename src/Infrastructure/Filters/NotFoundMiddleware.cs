130a36af8d123d916694db15de77e4c19c6c6b9f
Young <iamlovedit@163.com>
2024-04-30 11:19:00 +0800

    added idempotency filter

---
+++ src/Infrastructure/Filters/NotFoundMiddleware.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Roller.Infrastructure.Utils;
+
+namespace Roller.Infrastructure.Filters;
+
+public class NotFoundMiddleware(RequestDelegate next)
+{
+    public async Task InvokeAsync(HttpContext context)
+    {
+        await next(context);
+
+        if (context.Response.StatusCode == 404)
+        {
+            var message = new MessageData(false, $"路径: {context.Request.Path.Value} 不存在", 404);
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = 200;
+            await context.Response.WriteAsync(message.Serialize());
+        }
+    }
+}
