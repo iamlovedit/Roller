26c41dde71bf05132dd1916b728ec06d691d4890
Young <iamlovedit@163.com>
2024-08-16 11:13:10 +0800

    added json extension

---
+++ src/Infrastructure/Utils/JsonExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace Roller.Infrastructure.Utils;
+
+public static class JsonExtensions
+{
+    static JsonExtensions()
+    {
+        JsonConvert.DefaultSettings = () => new JsonSerializerSettings()
+        {
+            ContractResolver = new CamelCasePropertyNamesContractResolver(),
+            DateFormatString = "yyyy-MM-dd HH:mm:ss",
+            DateTimeZoneHandling = DateTimeZoneHandling.Local,
+            NullValueHandling = NullValueHandling.Ignore,
+            ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+        };
+    }
+
+    public static string Serialize(this object obj)
+    {
+        return JsonConvert.SerializeObject(obj);
+    }
+
+    public static T Deserialize<T>(this string json)
+    {
+        return JsonConvert.DeserializeObject<T>(json);
+    }
+}
