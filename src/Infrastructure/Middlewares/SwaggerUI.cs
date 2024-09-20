d016c7a7e9022631f0a41a35c0d1d4aab784bea5
Young <iamlovedit@163.com>
2024-09-20 11:05:09 +0800

    added swagger generate extension

---
+++ src/Infrastructure/Middlewares/SwaggerUI.cs
@@ -0,0 +1,23 @@
+using Asp.Versioning.ApiExplorer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Roller.Infrastructure.Middlewares;
+
+public static class SwaggerUI
+{
+    public static void UseVersionedSwaggerUI(this IApplicationBuilder app)
+    {
+        ArgumentNullException.ThrowIfNull(app);
+        var apiVersionDescriptionProvider =
+            app.ApplicationServices.GetRequiredService<IApiVersionDescriptionProvider>();
+        app.UseSwaggerUI(options =>
+        {
+            foreach (var description in apiVersionDescriptionProvider.ApiVersionDescriptions)
+            {
+                options.SwaggerEndpoint($"/swagger/{description.GroupName}/swagger.json",
+                    description.GroupName.ToUpperInvariant());
+            }
+        });
+    }
+}
