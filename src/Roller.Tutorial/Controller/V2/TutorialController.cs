65cc67d7dd5eac147ca8900c55823545010f50d3
Young <iamlovedit@163.com>
2024-09-20 11:06:55 +0800

    updated tutorial

---
+++ src/Roller.Tutorial/Controller/V2/TutorialController.cs
@@ -0,0 +1,17 @@
+﻿using Asp.Versioning;
+using Microsoft.AspNetCore.Mvc;
+using Roller.Infrastructure;
+
+namespace Roller.Tutorial.Controller.V2;
+
+[ApiVersion("2.0")]
+[Route("tutorial/{version:apiVersion}")]
+public class TutorialController(ILogger<TutorialController> logger) : RollerControllerBase
+{
+    [HttpGet("hello")]
+    public async Task<MessageData<string>> HelloAsync()
+    {
+        logger.LogInformation("hello world");
+        return await Task.Run(() => Success<string>("hello world"));
+    }
+}
