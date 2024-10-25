28af77767a942a2b2c22d70086dde8e2f2102afb
Young <iamlovedit@163.com>
2024-10-25 17:02:25 +0800

    update tutorial project

--- src/Roller.Tutorial/Controller/V1/TutorialController.cs
+++ src/Roller.Tutorial/Controller/V1/TutorialController.cs
@@ -2,12 +2,13 @@
 using Microsoft.AspNetCore.Mvc;
 using Roller.Infrastructure;
 using Roller.Infrastructure.Attributes;
+using Roller.Infrastructure.EventBus;
 
 namespace Roller.Tutorial.Controller.V1;
 
 [ApiVersion("1.0")]
 [Route("tutorial/{version:apiVersion}")]
-public class TutorialController(ILogger<TutorialController> logger) : RollerControllerBase
+public class TutorialController(ILogger<TutorialController> logger, IEventBus eventBus) : RollerControllerBase
 {
     [HttpGet("hello")]
     public async Task<MessageData<string>> HelloAsync()
@@ -21,6 +22,13 @@ public class TutorialController(ILogger<TutorialController> logger) : RollerCont
     public async Task<MessageData> TestAsync([FromBody] Account account)
     {
         return Success();
+    }
+    
+    [HttpPost("send")]
+    public IActionResult SendMessage([FromBody] string message)
+    {
+        eventBus.Publish(new MessageSentEvent { Message = message });
+        return Ok("Message sent.");
     }
 }
 
--- src/Roller.Tutorial/Program.cs
+++ src/Roller.Tutorial/Program.cs
@@ -1,10 +1,15 @@
+using Roller.Infrastructure.EventBus;
+using Roller.Infrastructure.EventBus.RabbitMQ;
 using Roller.Infrastructure.Middlewares;
 using Roller.Infrastructure.SetupExtensions;
+using Roller.Tutorial;
 
 var builder = WebApplication.CreateBuilder(args);
-
+builder.Services.AddRollerRabbitMQEventBus(builder.Configuration);
+builder.Services.AddTransient<MessageSentEventHandler>();
 var app = builder
     .AddInfrastructureSetup()
     .Build();
-
+var eventBus = app.Services.GetRequiredService<IEventBus>();
+eventBus.Subscribe<MessageSentEvent, MessageSentEventHandler>();
 app.UseInfrastructure();
