96caa4e616fd4905fd6f5eef456d0f016f0cc4e8
Young <iamlovedit@163.com>
2024-10-26 11:33:20 +0800

    removed newton json

--- src/Infrastructure.EventBus/IntegrationEvent.cs
+++ src/Infrastructure.EventBus/IntegrationEvent.cs
@@ -1,15 +1,12 @@
-using Newtonsoft.Json;
-
 namespace Roller.Infrastructure.EventBus;
 
-[method: JsonConstructor]
 public class IntegrationEvent(Guid id, DateTime createdDate)
 {
     public IntegrationEvent() : this(Guid.NewGuid(), DateTime.UtcNow)
     {
     }
 
-    [JsonProperty] public Guid Id { get; private set; } = id;
+    public Guid Id { get; private set; } = id;
 
-    [JsonProperty] public DateTime CreatedDate { get; private set; } = createdDate;
+    public DateTime CreatedDate { get; private set; } = createdDate;
 }
