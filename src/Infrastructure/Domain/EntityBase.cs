29396519853002e32f3813ff92c172e1d87cbab7
Young <iamlovedit@163.com>
2024-11-06 15:22:29 +0800

    updated entity model

--- src/Infrastructure/Domain/EntityBase.cs
+++ src/Infrastructure/Domain/EntityBase.cs
@@ -1,8 +1,11 @@
-﻿namespace Roller.Infrastructure.Domain;
+﻿using SqlSugar;
 
+namespace Roller.Infrastructure.Domain;
+
+[SugarTable]
 public abstract class EntityBase<T> : IEntityBase<T> where T : IEquatable<T>
 {
-    public T Id { get; set; }
+    [SugarColumn(IsPrimaryKey = true)] public T Id { get; set; }
 
     public DateTime CreatedDate { get; set; }
 
