57ccd41890bc33e6d6ccfe4e5bdb811896d5ba95
Young <iamlovedit@163.com>
2024-11-10 10:30:25 +0800

    update gitignore

--- src/Infrastructure/SetupExtensions/MongoDbSetup.cs
+++ src/Infrastructure/SetupExtensions/MongoDbSetup.cs
@@ -14,16 +14,30 @@ public static class MongoDbSetup
 
         ArgumentNullException.ThrowIfNull(configuration);
 
-        var mongoDbOptions = configuration.GetSection(MongoDbOptions.Name).Get<MongoDbOptions>();
+        var mongoDbOptions = configuration.GetSection(MongoDbOptions.SectionName).Get<MongoDbOptions>();
         if (mongoDbOptions is null || !mongoDbOptions.Enable)
         {
             return services;
         }
 
-        services.TryAddSingleton<IMongoDatabase>(_ =>
-            new MongoClient(mongoDbOptions.ConnectionString)
-                .GetDatabase(mongoDbOptions.Database));
+        var host = configuration[MongoDbOptions.MongoHost] ?? mongoDbOptions.Host;
+        ArgumentException.ThrowIfNullOrEmpty(host);
 
+        var user = configuration[MongoDbOptions.MongoUser] ?? mongoDbOptions.User;
+        ArgumentException.ThrowIfNullOrEmpty(user);
+
+        var password = configuration[MongoDbOptions.MongoPassword] ?? mongoDbOptions.Password;
+        ArgumentException.ThrowIfNullOrEmpty(password);
+
+        var port = configuration[MongoDbOptions.MongoPort] ?? mongoDbOptions.Port;
+        ArgumentException.ThrowIfNullOrEmpty(port);
+
+        var database = configuration[MongoDbOptions.MongoDatabase] ?? mongoDbOptions.Database;
+        ArgumentException.ThrowIfNullOrEmpty(database);
+        var connectionString = $"mongodb://{user}:{password}@{host}:{port}/{database}";
+        services.TryAddSingleton<IMongoDatabase>(_ =>
+            new MongoClient(connectionString)
+                .GetDatabase(mongoDbOptions.Database));
         services.TryAddScoped(typeof(IMongoRepositoryBase<,>), typeof(MongoRepositoryBase<,>));
         return services;
     }
